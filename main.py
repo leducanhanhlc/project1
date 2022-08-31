dfb2e0e1406310568b611860a9afac60783ed2c5
LeDucAnh <leducanhanhlc@gmail.com>
2022-08-31 09:28:26 +0700

    feat: add login and register

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-from flask import Flask, redirect, url_for, render_template, request, flash, send_file
-from flask_simplelogin import SimpleLogin
-from flask_simplelogin import login_required
+from flask import Flask, redirect, url_for, render_template, request, flash, send_file, session
 from wtforms import Form, DateField, StringField, validators
 import requests
 from fpdf import FPDF
+from flask_sqlalchemy import SQLAlchemy
 
 pdf_file_name = 'output.pdf'
 
 #Init Flask
-app = Flask(__name__, template_folder='./')
+app = Flask(__name__, template_folder='./templates')
 
-#Setting config
-app.config['SECRET_KEY'] = 'something-secret'
-app.config['SIMPLELOGIN_USERNAME'] = 'admin'
-app.config['SIMPLELOGIN_PASSWORD'] = 'admin'
+#Init database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
-#Init simple login
-SimpleLogin(app)
+#Define user table in database
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+#Generate report form
 class FormUseToGenerateMonthlyReport(Form):
     text_field = StringField('Text Field', [validators.InputRequired(), validators.Length(min=0, max=50)])
     date_field_1 = DateField('Date Field 1', [validators.InputRequired()])
@@ -25,21 +31,55 @@ class FormUseToGenerateMonthlyReport(Form):
 
 #Create homepage route
 @app.route("/", methods=['GET', 'POST'])
-
-#Force user login to access homepage
-@login_required
-
-#Content of homepage
+#Callback function
 def report():
-    form = FormUseToGenerateMonthlyReport()
-    print("POST LÊLELELádas")
+    if session.get('logged_in'):
+        form = FormUseToGenerateMonthlyReport()
+        if request.method == 'POST':
+            #Generate monthly report pdf file
+            generate_pdf_file(request.form["text_field"], request.form["date_field_1"], request.form["date_field_2"])
+            #flash('Thanks for registering')
+            #Send monthly report pdf file to client. Client will download this file.
+            return send_file(pdf_file_name, mimetype='application/force-download')
+        return render_template('generatemonthlyreportpage.html', form=form)
+    else:
+        return render_template('index.html', message="Hello!")
+    
+#Register route
+@app.route('/register/', methods=['GET', 'POST'])
+#Callback function
+def register():
     if request.method == 'POST':
-        #Generate monthly report pdf file
-        generate_pdf_file(request.form["text_field"], request.form["date_field_1"], request.form["date_field_2"])
-        #flash('Thanks for registering')
-        #Send monthly report pdf file to client. Client will download this file.
-        return send_file(pdf_file_name, mimetype='application/force-download')
-    return render_template('generatemonthlyreportpage.html', form=form)
+        try:
+            db.session.add(User(username=request.form['username'], password=request.form['password']))
+            db.session.commit()
+            return redirect(url_for('login'))
+        except:
+            return render_template('index.html', message="User Already Exists")
+    else:
+        return render_template('register.html')
+
+#Login route
+@app.route('/login/', methods=['GET', 'POST'])
+#Callback function
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        u = request.form['username']
+        p = request.form['password']
+        data = User.query.filter_by(username=u, password=p).first()
+        if data is not None:
+            session['logged_in'] = True
+            return redirect(url_for('report'))
+        return render_template('index.html', message="Incorrect Details")
+
+#Logout route
+@app.route('/logout', methods=['GET', 'POST'])
+#Callback function
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('report'))
 
 #Generate pdf file. PS: Code cua thay viet vao day
 def generate_pdf_file(text, date_1, date_2):
@@ -49,3 +89,6 @@ def generate_pdf_file(text, date_1, date_2):
     pdf.cell(40, 10, text + "##############" + date_1 + "##############" + date_2)
     pdf.output(pdf_file_name, 'F')
     
+#Main
+app.secret_key = "ThisIsNotASecret:p"
+db.create_all()
